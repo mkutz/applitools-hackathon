9bcf24ced245c13082f2e826c0c063cc7dbd6346
mkutz <michael.kutz@rewe-digital.com>
2019-11-12 09:51:52 +0100

    Add testWithTestcontaiers task for CI

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,11 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.testcontainers.containers.BrowserWebDriverContainer
 
 baseUrl = 'https://demo.applitools.com/hackathon.html'
 
 driver = {
-    BrowserWebDriverContainer container = new BrowserWebDriverContainer()
-            .withCapabilities(new ChromeOptions())
-    container.start()
-    return container.webDriver
+    WebDriverManager.chromedriver().setup()
+    return new ChromeDriver()
+}
+
+envrionments {
+    chromeTestcontainers {
+        BrowserWebDriverContainer container = new BrowserWebDriverContainer()
+                .withCapabilities(new ChromeOptions())
+        container.start()
+        return container.webDriver
+    }
 }
